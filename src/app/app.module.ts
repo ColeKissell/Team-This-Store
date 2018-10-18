7fa2b39b76a1fbfa8f9540eebfd232680e4424fa
Conner Donohoo <donohoo13@Conners-MacBook-Air.local>
2018-10-18 15:12:30 -0600

    10-18-18 mid-day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+// import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-// import { Module } from 'module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +24,18 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     ContactComponent,
     FooterComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
+    // FormsModule,
+    NgbModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterUserComponent },
+      { path: 'registerUser', component: RegisterUserComponent },
       { path: 'contact', component: ContactComponent },
+      { path: 'products', component: ProductsComponent },
     ])
   ],
   providers: [],
