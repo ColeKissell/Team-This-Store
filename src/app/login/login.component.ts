2f8914774e01297290e598acec60155cd45fc5e5
Conner Donohoo <donohoo13@Conners-MacBook-Air.local>
2018-10-19 10:23:10 -0600

    added iffy validation to login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // model: {
+  //   email = {},
+  //   password = {},
+  // }
+  model: any = {}
+  
 
-  constructor() { }
+constructor() { }
 
   ngOnInit() {
-  }
 
+  }
 }
